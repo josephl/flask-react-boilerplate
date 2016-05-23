32e94016b0e8db4e2412040bdb75b310e361d341
Joseph Lee <joe.lee.three.thousand@gmail.com>
2016-05-22 19:06:00 -0700

    Remove Kitten API blueprint, references from server side

--- server/app.py
+++ server/app.py
@@ -1,19 +1,20 @@
 import os
-from flask.ext.sqlalchemy import SQLAlchemy
 from logging import StreamHandler
 from sys import stdout
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
+
 def create_app():
-    from api.kittens import kittens_api
     from views.index import index_view
 
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
-    app.register_blueprint(kittens_api.blueprint, url_prefix='/api')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.register_blueprint(index_view)
 
     db.init_app(app)
